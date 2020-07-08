c858fcb5c20b07aeb275b70ad60ccbd7fc675698
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-07-08 16:00:59 +0700

    home page view refactored and now used template

--- superlists/settings_template.py
+++ superlists/settings_template.py
@@ -28,6 +28,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
+    'lists',
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
