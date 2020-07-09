a593b0e9561dd465e80fe88a98f901e9150aa0f3
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-07-09 17:12:47 +0700

    test_only_saves_items_when_necessary

--- lists/tests.py
+++ lists/tests.py
@@ -16,8 +16,18 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         """тест: можно сохранить POST-запрос"""
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_only_saves_items_when_necessary(self):
+        """тест: сохраняет элементы, только когда нужно"""
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
     """тест модели элемента списка"""
