faf0f5d43d48683f7c4c7004b84ffee85ae97dc8
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-07-15 18:05:23 +0700

    time.sleep replaced by cycle with try/except

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,10 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.common.exceptions import WebDriverException
+
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -15,11 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
         """демонтаж"""
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        """подтверждение строки в конце списка"""
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        """ожидание строки в конце списка"""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
@@ -46,7 +58,7 @@ class NewVisitorTest(LiveServerTestCase):
         # "1. Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
 
         #  Текстовое поле по-прежнему предлагает добавить элемент списка.
         # Пользователь вводит "Сделать мушку из павлиньих перьев"
@@ -57,13 +69,12 @@ class NewVisitorTest(LiveServerTestCase):
         )
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
 
         # Страница вновь обновляется, и теперь показывает оба элемента списка
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
-        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         # Сайт запоминает этот список. Сгенерирован уникальный URL, и об этом выводится текст с объснением
         self.fail('Закончить тест!')
