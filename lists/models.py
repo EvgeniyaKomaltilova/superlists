917adefd36ebb0187b5bea3620716826c83d470b
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-07-17 16:24:12 +0700

    List model

--- lists/models.py
+++ lists/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Item(models.Model):
     """элемент списка"""
     text = models.TextField(default='')
+    list = models.ForeignKey('List', default=None, on_delete=models.CASCADE)
+
+
+class List(models.Model):
+    """список"""
+    name = models.CharField(max_length=255)
